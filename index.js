2814cd5a6404b5abca8abb32216aa85bec6c8483
Sajith Sampath <70060290+sajithcode@users.noreply.github.com>
2024-02-11 13:38:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//add to function
+function addtocart(){
+  alert("The item added to cart  successfully!");
+}
+
+function viewcart(){
+  alert("Sorry, No items there.");
+}
+
+
+
+
+
