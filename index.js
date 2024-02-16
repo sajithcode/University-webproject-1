5ee3fc7b68dfc3094416cf63050c1da502bbe79a
sajithcode <srs22580@gmail.com>
2024-02-16 08:07:32 +0530

    Prices are changed

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//add to function
+function payment(){
+  alert("The payment placed successfully!");
+}
+
+function viewcart(){
+  alert("Sorry, No items there. First Add to cart items.");
+}
+
+
+
+
+
